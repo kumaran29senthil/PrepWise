e3e788713c85a745a645fbf299ce49256b99d2c7
Kummaran <kumaransenthilarasu@gmail.com>
2026-05-12 23:44:44 +0530

    fix standalone azure build

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   eslint: {
+  output: "standalone",
+
+  eslint: {
     ignoreDuringBuilds: true,
   },
+
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;
