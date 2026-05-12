aebfab009d76f6ea73e7b74104498286a6fb2f4e
Kummaran <kumaransenthilarasu@gmail.com>
2026-05-13 01:15:09 +0530

    Add standalone Next.js build for Azure

---
+++ .next/static/chunks/app/(root)/interview/[id]/page-b6b040f07d784b22.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[182],{4134:(e,s,n)=>{Promise.resolve().then(n.bind(n,7409)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[424,356,531,720,584,409,441,255,358],()=>e(e.s=4134)),_N_E=e.O()}]);
